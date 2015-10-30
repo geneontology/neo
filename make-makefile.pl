4c2ff7b5de2fe6414d80f3cf87014bcf5217e814
Chris Mungall <cjm@berkeleybop.org>
2015-10-30 15:57:22 -0700

    first pass at NEO. Fixes https://github.com/geneontology/noctua/issues/215

--- make-makefile.pl
+++ make-makefile.pl
@@ -1,12 +1,21 @@
 #!/usr/bin/perl
 use strict;
 my %smap = (
-    Scer => 'sgd'
+    Scer => 'sgd',
+    Mmus => 'mgi',
+    Hsap => 'goa_ref_human'
     );
 
+my @tgts = ();
 foreach (keys %smap) {
+    my $tgt = "neo-$_.obo";
+    push(@tgts, $tgt);
     my $n = $smap{$_};
-    print "neo-$_.obo: $n.gaf.gz\n";
+    print "$tgt: $n.gaf.gz\n";
     print "\tgzip -dc \$< | gaf2obo.pl -s $_ > \$@.tmp && mv \$@.tmp \$@\n\n";
-    
 }
+
+print "all_obo: @tgts\n";
+
+print "neo.obo: @tgts\n";
+print "\towltools --create-ontology http://purl.obolibrary.org/obo/go/noctua/neo.owl \$^ --merge-support-ontologies  -o -f obo \$@\n";
