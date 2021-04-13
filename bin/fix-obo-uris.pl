bc93eef7fc23b8050801d25235a53bd7b69fd4e7
Jim Balhoff <jim@balhoff.org>
2021-04-13 17:24:59 -0400

    Tweak obo-uris hack to handle hash injected by OBO parser.

--- bin/fix-obo-uris.pl
+++ bin/fix-obo-uris.pl
@@ -6,7 +6,8 @@ s@http://purl.obolibrary.org/obo/TAIR_locus%3A@http://identifiers.org/tair.locus
 # gramene IDs are actually uniprot...
 s@http://purl.obolibrary.org/obo/GR_protein_@http://identifiers.org/uniprot/@g;
 s@http://purl.obolibrary.org/obo/dictyBase#_@http://identifiers.org/dictybase.gene/@g;
-s@http://purl.obolibrary.org/obo/(ZFIN|FB|WB|SGD|PomBase|RGD|MGI|Xenbase|UniProtKB|TAIR:\s+)_@"http://identifiers.org/".fix($1)."/"@eg;
+# The optional hash in the match deals with some odd OWLAPI OBO parser behavior
+s@http://purl.obolibrary.org/obo/(ZFIN|FB|WB|SGD|PomBase|RGD|MGI|Xenbase|UniProtKB|TAIR:\s+)#?_@"http://identifiers.org/".fix($1)."/"@eg;
 
 s@http://identifiers.org/mgi/MGI%3A@http://identifiers.org/mgi/MGI:@g;
 
